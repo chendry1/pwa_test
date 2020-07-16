5944dc144383080eb0eab9767dcfc53239d10ccb
chendry1 <ofesenko@reviversoft.com>
2020-07-16 16:25:47 +0300

    [master] service worker correction

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,4 +1,23 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.1/workbox-sw.js');
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "index.html";
+
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/password_rank.js',
+  '/password_generation.js',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -6,4 +25,35 @@ self.addEventListener("message", (event) => {
   }
 });
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(filesToCache);
+        return cachedResp;
+      }
+    })());
+  }
+});
