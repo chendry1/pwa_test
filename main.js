8b8a054c57d5c954dc2d0522756c0b5046f9894c
chendry1 <ofesenko@reviversoft.com>
2020-07-20 12:58:06 +0300

    [master] correction

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 window.onload = () => {  
-'use strict';     
-if ('serviceWorker' in navigator) {     
-navigator.serviceWorker  
-.register('./service-worker.js'); 
-} 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw-test/sw.js', {scope: './sw-test/'})
+  .then((reg) => {
+    // регистрация сработала
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch((error) => {
+    // регистрация прошла неудачно
+    console.log('Registration failed with ' + error);
+  });
+}
 }
