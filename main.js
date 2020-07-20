ccc2a661780b03007b4a490a11c65ddd966e876f
chendry1 <ofesenko@reviversoft.com>
2020-07-20 13:01:29 +0300

    [master] path correction

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = () => {  
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw-test/sw.js', {scope: './sw-test/'})
+  navigator.serviceWorker.register('./service-worker.js', {scope: './'})
   .then((reg) => {
     // регистрация сработала
     console.log('Registration succeeded. Scope is ' + reg.scope);
