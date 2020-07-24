1b5beaae5702a05c084b13fb7ce1f947a97ca402
chendry1 <ofesenko@reviversoft.com>
2020-07-24 21:52:27 +0300

    [master] correction

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,20 @@ const cacheName = 'hello-pwa';
 
 //importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
+var filesToCache = [
+  'index.html',
+  'standalone.html',
+  'manifest.json',
+  'main.js',
+  'password_rank.js',
+  'password_generation.js',
+  'icons/icon-128x128.png',
+  'icons/icon-144x144.png',
+  'icons/icon-152x152.png',
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png'
+];
+
 const CACHE = "pwabuilder-page";
 
 var installed = false;
