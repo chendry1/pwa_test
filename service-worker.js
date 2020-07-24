86d5923a560f887b85258f4b57a84ab382b4d075
chendry1 <ofesenko@reviversoft.com>
2020-07-24 20:49:31 +0300

    [master] work on service worker

--- service-worker.js
+++ service-worker.js
@@ -3,28 +3,14 @@ const cacheName = 'hello-pwa';
 
 //importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
-const isInStandaloneMode = () =>
-      (window.matchMedia('(display-mode: standalone)').matches) || (window.navigator.standalone) || document.referrer.includes('android-app://');
-
 const CACHE = "pwabuilder-page";
 
-var filesToCache = [
-  'index.html',
-  'standalone.html',
-  'manifest.json',
-  'main.js',
-  'password_rank.js',
-  'password_generation.js',
-  'icons/icon-128x128.png',
-  'icons/icon-144x144.png',
-  'icons/icon-152x152.png',
-  'icons/icon-192x192.png',
-  'icons/icon-512x512.png'
-];
+var installed = false;
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   console.log("install called");
+  installed = true;
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
@@ -39,7 +25,7 @@ self.addEventListener('fetch', function(event) {
   if( event.request.url == "https://chendry1.github.io/pwa_test/" )
   {
 	  console.log( "root folder request detected" );
-	  if( isInStandaloneMode() )
+	  if( installed )
 	  {
 		 console.log( "browser is in standalone mode" );
 		 request = new Request( "https://chendry1.github.io/pwa_test/standalone.html" );  
